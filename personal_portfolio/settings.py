b008ad3c4c413f626215feaa089c523892949713
Zion Tavera <zionkeenen.tavera@sun-asterisk.com>
2021-10-01 00:22:22 +0800

    new local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -129,3 +129,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("File Does not Exist")
